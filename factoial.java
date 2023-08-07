431932b21b838b31fba045b105ab6e81b0feff3e
MehakVarshney <122101735+MehakVarshney@users.noreply.github.com>
2023-08-07 10:36:54 +0530

    Add files via upload

---
+++ factoial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class factoial {
+    public static void main(String[] args) {
+        int i = 1;
+        int fact = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num = sc.nextInt();
+        while (i <= num) {
+            fact = fact * i;
+            i++;
+        }
+        System.out.println("The factorial is: " + fact);
+    }
+}
