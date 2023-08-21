2b348cfc5a0a0ca99c5fb9d6ca99c6fe564ee46d
MehakVarshney <122101735+MehakVarshney@users.noreply.github.com>
2023-08-21 18:55:39 +0530

    Add files via upload

---
+++ firstnnumber.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class firstnnumber {
+    public static void main(String[] args) {
+        int i = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the first number:");
+        int a = sc.nextInt();
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= a);
+    }
+
+}
