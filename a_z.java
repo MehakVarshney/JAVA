125bd10f26b22c763a199f99990df71c3bd6572a
MehakVarshney <122101735+MehakVarshney@users.noreply.github.com>
2023-09-14 09:21:52 +0530

    Add files via upload

---
+++ a_z.java
@@ -0,0 +1,9 @@
+public class a_z {
+    public static void main(String[] args) {
+        char i = 'A';
+        while (i <= 'Z') {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
